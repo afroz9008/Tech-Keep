2f498ac5d42e10425ef85d173608b182f64e2c81
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2020-08-16 03:18:28 -0700

    Add files via upload

---
+++ precache-manifest.321a4639ddd5ea92ec4382e6579d28b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83082a9be4889ab490157b0eaa9ecd8d",
+    "url": "/Tech-Keep/index.html"
+  },
+  {
+    "revision": "16a234cea7ef77aa1c42",
+    "url": "/Tech-Keep/static/css/main.435d80d0.chunk.css"
+  },
+  {
+    "revision": "76630d566a44117b7c6d",
+    "url": "/Tech-Keep/static/js/2.6e6bdcb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tech-Keep/static/js/2.6e6bdcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a234cea7ef77aa1c42",
+    "url": "/Tech-Keep/static/js/main.ff38f756.chunk.js"
+  },
+  {
+    "revision": "2bb1bb1d3f1f65623fd0",
+    "url": "/Tech-Keep/static/js/runtime-main.3fbdabdd.js"
+  }
+]);
